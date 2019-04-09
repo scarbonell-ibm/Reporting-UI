cb5db7327385b8be9e4808f88b9d0bacd4fc6c0b
shemlick <43967984+shemlick@users.noreply.github.com>
2019-04-09 14:52:35 -0400

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -4,10 +4,8 @@ rapid7="https://54.236.84.177:3780"
 result=$(curl -k -X POST \
   ${rapid7}/api/3/assets/search \
   -H 'Accept: application/json' \
-  -H 'Authorization: Basic U2lkZGhhcnRoX0Nob3VkaGFyeTpTaWRAaGNs' \
   -H 'Content-Type: application/json' \
-  -H 'Postman-Token: 0bd2544f-f2a3-460f-ae59-eb5bb96a0923' \
-  -H 'cache-control: no-cache' \
+  -u Siddharth_Choudhary:Sid@hcl
   -d '{
 	"filters": [{ 
 		"field": "ip-address", 
