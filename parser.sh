cd7bdb6f66e05dbfba4d896ccba999ee2dd8c9fc
shemlick <43967984+shemlick@users.noreply.github.com>
2019-04-09 14:16:09 -0400

    Update parser.sh

--- parser.sh
+++ parser.sh
@@ -1,1 +1,20 @@
-echo "test"
+IP="10.3.0.117"
+echo $IP
+
+result = curl -X POST \
+  https://54.236.84.177:3780/api/3/assets/search \
+  -H 'Accept: application/json' \
+  -H 'Authorization: Basic U2lkZGhhcnRoX0Nob3VkaGFyeTpTaWRAaGNs' \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 0bd2544f-f2a3-460f-ae59-eb5bb96a0923' \
+  -H 'cache-control: no-cache' \
+  -d '{
+	"filters": [{ 
+		"field": "ip-address", 
+		"operator": "is", 
+		"value": "10.3.0.117"
+	}],
+	"match": "all"
+}'
+
+echo $result
